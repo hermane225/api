aee0793c288b39a35a22f0886e71fb4af7a5fdb7
hermane225 <mj2190175@gmail.com>
2026-01-16 14:46:19 +0000

    add check/:codeStr

--- src/routes/codes.routes.js
+++ src/routes/codes.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { verifyToken, permit } from "../middleware/auth.js";
-import { generateCodes, listCodes, assignCode } from "../controllers/code.controller.js";
+import { generateCodes, listCodes, assignCode, checkCodeStatus } from "../controllers/code.controller.js";
 
 const router = express.Router();
 
 router.post("/generate", verifyToken, permit("admin","agent"), generateCodes);
 router.get("/", verifyToken, listCodes);
+router.get("/check/:codeStr", checkCodeStatus);
 router.put("/:id/assign", verifyToken, permit("agent","admin"), assignCode);
 
 export default router;
